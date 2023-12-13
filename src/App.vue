ea7cafb03c0f7a1f0e8bb11c64f14fe0d2cd861a
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-12-13 15:15:00 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>App Component</h1>
+    <input type="text" placeholder="Say Something" v-model="msg">
+    <Products :greeting="msg"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Products from './components/Products.vue';
 
 export default {
   name: 'App',
+  data(){
+    return {
+      msg:''
+    } 
+  },
   components: {
-    HelloWorld
-  }
+    Products
+}
 }
 </script>
 
@@ -22,5 +30,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container{
+  background-color: crimson;
 }
 </style>
---
+++ src/components/Products.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <h2>Child Component</h2>
+        <p>{{ greeting }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ProductComp',
+        props: ['greeting']
+    }
+</script>
+
+<style scoped>
+.container{
+    background-color: chocolate;
+}
+</style>
